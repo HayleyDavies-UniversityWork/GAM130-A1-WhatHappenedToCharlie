a38f873252997ce80f8c33c140bfac358e61e12a
Hayley Davies <cd230099@falmouth.ac.uk>
2021-01-26 18:22:08 +0000

    added basic sliding puzzle back end

---
+++ Assets/Scripts/SlidingPuzzle/SlidingPuzzle.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlidingPuzzle : MonoBehaviour {
+    public SlidingPuzzleBoard puzzleBoard;
+    public SlidingPuzzleBoard puzzleBoardSolution;
+
+    // Start is called before the first frame update
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+
+    void DisplayBoard() {
+
+    }
+}
+
+[System.Serializable]
+public class SlidingPuzzleBoard {
+
+    [System.Serializable] public struct column {
+        public Sprite[] row;
+    }
+
+    public column[] board;
+    public int boardSize;
+}
