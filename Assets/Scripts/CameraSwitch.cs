381dad70fd22ec837c47d6539daf775009b93971
artemijsem <AS247317@falmouth.ac.uk>
2021-01-27 19:23:19 +0200

    Pushed a prototype version. P.S. I could've pushed wrong files as well so please report back if anything doesn't work

---
+++ Assets/Scripts/CameraSwitch.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class CameraSwitch : MonoBehaviour
+{
+    public GameObject CurrentCamera;
+    public GameObject[] Cameras;
+
+    private void NextCameraSwitch()
+    {
+        Cameras[0].SetActive(false);
+        Cameras[1].SetActive(true);
+        CurrentCamera = Cameras[1];
+    }
+    private void PreviousCameraSwitch()
+    {
+        Cameras[1].SetActive(false);
+        Cameras[0].SetActive(true);
+        CurrentCamera = Cameras[0];
+    }
+    private void OnTriggerExit(Collider col)
+    {
+
+        if (col.gameObject.CompareTag("Player"))
+        {
+            if(CurrentCamera == Cameras[1])
+            {
+                PreviousCameraSwitch();
+            }
+            else
+            {
+                NextCameraSwitch();
+            }
+        }
+        
+    }
+
+
+}
---
+++ Assets/Scripts/Inventory.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Inventory : MonoBehaviour
+{
+
+    public Dictionary<string, Item> inventory;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        inventory = new Dictionary<string, Item>();
+    }
+
+    public void AddItemToDictionary(Item item)
+    {
+        inventory.Add(item.Name, item);
+    }
+
+    public bool ItemExists(string itemName)
+    {
+        if (inventory.ContainsKey(itemName))
+        {
+            return true;
+        }
+        return false;
+    }
+
+    public void Remove(string itemName)
+    {
+        inventory.Remove(itemName);
+    }
+
+    [CreateAssetMenu(fileName = "Inventory Item", menuName = "Inventory/Item", order = 1)]
+    public class Item : ScriptableObject
+    {
+        public Image Icon { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+        public Item(string name, string description, Image icon)
+        {
+            Icon = icon;
+            Name = name;
+            Description = description;
+        }
+    }
+}
