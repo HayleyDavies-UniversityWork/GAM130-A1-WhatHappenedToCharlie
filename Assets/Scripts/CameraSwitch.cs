fcdf2b3d55f2a52e16565fdff1d31676bc0eb187
artemijsem <AS247317@falmouth.ac.uk>
2021-02-03 19:26:27 +0200

    Added Item Swapping as Ryan told. Added some things to CameraSwitch.cs including comments. P.S. Got rid of that cube in Bedroom

--- Assets/Scripts/CameraSwitch.cs
+++ Assets/Scripts/CameraSwitch.cs
@@ -5,26 +5,37 @@ using UnityEngine;
 
 public class CameraSwitch : MonoBehaviour
 {
+    //Camera object which is being used at the moment
     public GameObject CurrentCamera;
-    public GameObject[] Cameras;
 
+    //Camera array list which should include both cameras
+    public GameObject[] Cameras = new GameObject[2];
+
+    //Event handler which triggers fucntions when camera being switched (eg ItemSwap.cs)
+    public GameObject EventHandler;
+
+    //Fucntion for switching camera forth
     private void NextCameraSwitch()
     {
         Cameras[0].SetActive(false);
         Cameras[1].SetActive(true);
         CurrentCamera = Cameras[1];
     }
+    //Fucntion for switching camera back
     private void PreviousCameraSwitch()
     {
         Cameras[1].SetActive(false);
         Cameras[0].SetActive(true);
         CurrentCamera = Cameras[0];
     }
+
+    //Function being called when Player goes through Collider
     private void OnTriggerExit(Collider col)
     {
 
         if (col.gameObject.CompareTag("Player"))
         {
+            //Detecting to which camera next switch should be
             if(CurrentCamera == Cameras[1])
             {
                 PreviousCameraSwitch();
@@ -32,6 +43,12 @@ public class CameraSwitch : MonoBehaviour
             else
             {
                 NextCameraSwitch();
+            }
+
+            //TEST: When player leaves Bedroom items will be swapped
+            if(CurrentCamera.name != "BedroomCamera")
+            {
+                EventHandler.GetComponent<ItemSwap>().SwapItems();
             }
         }
         
---
+++ Assets/Scripts/ItemSwap.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemSwap : MonoBehaviour
+{
+    //Initial item which we will be swapping
+    public GameObject StartItem;
+
+    //Item which will replace initial one
+    public GameObject SwapItem;
+   
+
+    public void SwapItems()
+    {
+        //Check if the item isn't destroyed
+        if (StartItem != null)
+        {
+            GameObject SwappedItem = Instantiate(SwapItem) as GameObject;
+
+            SwappedItem.transform.position = StartItem.transform.position;
+
+            Destroy(StartItem);
+
+        }
+    }
+
+
+
+
+
+}
