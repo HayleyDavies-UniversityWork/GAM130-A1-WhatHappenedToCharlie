aedd092d60793d032ec762e6747bcf00bd569a57
mateusz <mo248618@falmouth.ac.uk>
2021-02-04 10:58:20 +0000

    I added the walking script to the robot kyle prefab he now walks when the "Up" key is pressed. We still have to add movement.

---
+++ Assets/Scripts/Walk.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Walk : MonoBehaviour
+{
+    public Animator anim;
+    // Update is called once per frame
+    void Update()
+    {
+        anim.SetFloat("Vertical", Input.GetAxis("Vertical"));
+        anim.SetFloat("Horizontal", Input.GetAxis("Horizontal"));
+    }
+}
