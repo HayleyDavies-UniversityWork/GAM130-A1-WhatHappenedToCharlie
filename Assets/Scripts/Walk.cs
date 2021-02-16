b4759dfeb62beaf55a24e7eb3514b35b0b2c4a7d
Artemi Sementsenko <as247317@falmouth.ac.uk>
2021-02-16 13:07:02 +0200

    Programmed the PickUp animation. If you press E the PickUp animation will play.

--- Assets/Scripts/Walk.cs
+++ Assets/Scripts/Walk.cs
@@ -5,10 +5,19 @@ using UnityEngine;
 public class Walk : MonoBehaviour
 {
     public Animator anim;
+    
     // Update is called once per frame
     void Update()
     {
         anim.SetFloat("Vertical", Input.GetAxis("Vertical"));
         anim.SetFloat("Horizontal", Input.GetAxis("Horizontal"));
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            anim.SetTrigger("PickUp");
+        }
+        else
+        {
+            anim.ResetTrigger("PickUp");
+        }
     }
 }
