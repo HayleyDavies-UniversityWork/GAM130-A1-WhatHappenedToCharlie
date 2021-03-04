d7feec6c7c594007873a7062792abd444ae60e2e
Hayley Davies <cd230099@falmouth.ac.uk>
2021-03-04 13:32:03 +0000

    reverted Xav's changes to the CameraSwitch file

--- Assets/Scripts/CameraSwitch.cs
+++ Assets/Scripts/CameraSwitch.cs
@@ -2,9 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-
-public class CameraSwitch : MonoBehaviour
-{
+public class CameraSwitch : MonoBehaviour {
     //Camera object which is being used at the moment
     public GameObject CurrentCamera;
 
@@ -15,54 +13,35 @@ public class CameraSwitch : MonoBehaviour
     public GameObject EventHandler;
 
     //Fucntion for switching camera forth
-    private void NextCameraSwitch()
-    {
+    private void NextCameraSwitch() {
         Cameras[0].SetActive(false);
         Cameras[1].SetActive(true);
         CurrentCamera = Cameras[1];
     }
     //Fucntion for switching camera back
-    private void PreviousCameraSwitch()
-    {
+    private void PreviousCameraSwitch() {
         Cameras[1].SetActive(false);
         Cameras[0].SetActive(true);
         CurrentCamera = Cameras[0];
     }
 
     //Function being called when Player goes through Collider
-    private void OnTriggerExit(Collider col)
-    {
+    private void OnTriggerExit(Collider col) {
 
-        
-
-
-        if (col.gameObject.CompareTag("Player"))
-        {
-            
-
+        if (col.gameObject.CompareTag("Player")) {
             //Detecting to which camera next switch should be
-            if(CurrentCamera == Cameras[1])
-            {
+            if (CurrentCamera == Cameras[1]) {
                 PreviousCameraSwitch();
-                
-            }
-            else
-            {
+            } else {
                 NextCameraSwitch();
-                
             }
 
             //TEST: When player leaves Bedroom items will be swapped
-            if (CurrentCamera.name != "BedroomCamera")
-            {
+            if (CurrentCamera.name != "BedroomCamera") {
                 EventHandler.GetComponent<ItemSwap>().SwapItems();
-            }  
-       
+            }
         }
 
-        
-    
-    
     }
 
 }
