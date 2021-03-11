2ff8e85ba5d2cc3ab401758d0159d8152430bebb
HAYLEY DAVIES <cd230099@falmouth.ac.uk>
2021-03-11 12:47:03 +0000

    -Updated the code and working on a solution for interacting with the lock dials

---
+++ Assets/Scripts/Lock/Combination Lock/LockDial.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace CombinationLock {
+    public class LockDial : MonoBehaviour {
+        readonly int MAX_VALUE = 9;
+        int value;
+        int correctValue;
+
+        public void Initialize(ComboLockObject comboLockObject, int lockNumber) {
+            value = comboLockObject.startValues[lockNumber];
+            correctValue = comboLockObject.correctValues[lockNumber];
+        }
+
+        public bool isCorrectValue() {
+            if (value == correctValue)
+                return true;
+            return false;
+        }
+
+        public void MoveUpValue() {
+            value = (value + 1) % MAX_VALUE;
+        }
+
+        public void MoveDown() {
+            value = (value - 1) % MAX_VALUE;
+        }
+
+        public void OnMouse() {
+            MoveUpValue();
+        }
+    }
+}
