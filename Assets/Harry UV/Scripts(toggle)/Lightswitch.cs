b6e4e51b77d7abc4fd58305c9fdbafc887fb759a
HARRY KING <hk245214@falmouth.ac.uk>
2021-03-25 11:35:45 +0000

    Shader and models
    added shader that reacts to light and a few models for the office

---
+++ Assets/Harry UV/Scripts(toggle)/Lightswitch.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lightswitch : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //toggleroom light off
+        //allow uvflashlight only on when off
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Harrysbadmodels/lightbulb.ma
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:c9cd7cb47b0167aff915cb3eb733e721becf54fc31805d29af2cba8577c6f09f
+size 142100
