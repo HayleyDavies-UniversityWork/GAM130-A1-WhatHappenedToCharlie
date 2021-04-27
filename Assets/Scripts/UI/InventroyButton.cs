7df05328feb9683ff49d36d5c00286c483ac634a
artemijsem <AS247317@falmouth.ac.uk>
2021-04-27 20:50:49 +0300

    Added UI prefab and InventroyButton script

---
+++ Assets/Scripts/UI/InventroyButton.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InventroyButton : MonoBehaviour
+{
+    public Image image;
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.I))
+        {
+            OpenInventroy();
+        }
+    }
+    public void OpenInventroy()
+    {
+        if(image.enabled == true)
+        {
+            image.enabled = false;
+        }
+        else
+        {
+            image.enabled = true;
+        }
+    }
+}
