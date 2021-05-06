64b313be951792cd30fe65088646bc2a4d55e0a6
Hayley Davies <cd230099@falmouth.ac.uk>
2021-05-06 16:22:47 +0100

    -Made the cameras follow the player - this works via CameraController.cs -Made the interaction ui show above the player and face the currently enabled camera. (needs work for sizing - will come later -Moved the bedroom camera - with cameras following, you could see outside the room - will check other cameras too. -Starting camera can now be changed - this will check which one is enabled (first) and keep that as the starting camera - fixes bug with bouncing

--- Assets/Scripts/CameraSwitch.cs
+++ Assets/Scripts/CameraSwitch.cs
@@ -2,54 +2,57 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[ExecuteInEditMode]
 public class CameraSwitch : MonoBehaviour {
     //Camera object which is being used at the moment
-    public GameObject CurrentCamera;
+    //public GameObject CurrentCamera;
 
     //Camera array list which should include both cameras
-    public GameObject[] Cameras = new GameObject[2];
+    //public GameObject[] Cameras = new GameObject[2];
 
     //Event handler which triggers fucntions when camera being switched (eg ItemSwap.cs)
-    public GameObject EventHandler;
+    //public GameObject EventHandler;
 
     //RoomAudio script to tell the audio manager the players location
-    public RoomAudio roomAudio;
+    //public RoomAudio roomAudio;
 
     //Second trigger which is being used to make sure that player went through the door
-    public GameObject Second_Trigger;
+    //public GameObject Second_Trigger;
 
-    Transform CamTarget;
+    public Transform CamTarget;
 
     void Start() {
-        CamTarget = GameObject.Find("CamTarget").GetComponent<Transform>();
+        // CamTarget = GameObject.Find("CamTarget").GetComponent<Transform>();
     }
 
-    //Fucntion for switching camera 
-    private void NextCameraSwitch() {
-        Cameras[0].SetActive(false);
-        //Cameras[0].GetComponent<RoomAudio>().StopAudio();
-        Cameras[1].SetActive(true);
-        //Cameras[1].GetComponent<RoomAudio>().PlayAudio();
-        CurrentCamera = Cameras[1];
-    }
+    /// OLD CODE
+    // //Fucntion for switching camera 
+    // private void NextCameraSwitch() {
+    //     Cameras[0].SetActive(false);
+    //     //Cameras[0].GetComponent<RoomAudio>().StopAudio();
+    //     Cameras[1].SetActive(true);
+    //     //Cameras[1].GetComponent<RoomAudio>().PlayAudio();
+    //     CurrentCamera = Cameras[1];
+    // }
 
     //Function being called when Player goes through Collider
-    private void OnTriggerExit(Collider col) {
-        isTriggered Second_Col = Second_Trigger.GetComponent<isTriggered>();
+    // private void OnTriggerExit(Collider col) {
+    //     isTriggered Second_Col = Second_Trigger.GetComponent<isTriggered>();
 
-        if (col.gameObject.name == "Player" && Second_Col.isEntered && col == GameObject.FindGameObjectWithTag("Player").GetComponent<CharacterController>()) {
-            // PLACE YOUT FUNCTIONS HERE XAV!!!!
-            NextCameraSwitch();
+    //     if (col.gameObject.name == "Player" && Second_Col.isEntered && col == GameObject.FindGameObjectWithTag("Player").GetComponent<CharacterController>()) {
+    //         // PLACE YOUT FUNCTIONS HERE XAV!!!!
+    //         NextCameraSwitch();
 
-            //Debug.Log("TEST");
-            //TEST: When player leaves Bedroom items will be swapped
-            if (CurrentCamera.name != "BedroomCamera") {
-                EventHandler.GetComponent<ItemSwap>().SwapItems();
-            }
-        }
-    }
-    private void FixedUpdate() {
-        transform.LookAt(CamTarget.transform);
+    //         //Debug.Log("TEST");
+    //         //TEST: When player leaves Bedroom items will be swapped
+    //         if (CurrentCamera.name != "BedroomCamera") {
+    //             EventHandler.GetComponent<ItemSwap>().SwapItems();
+    //         }
+    //     }
+    // }
+
+    private void Update() {
+        this.transform.forward = CamTarget.transform.position - this.transform.position;
     }
 
 }
