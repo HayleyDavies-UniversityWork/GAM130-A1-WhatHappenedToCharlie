76150d31c2c581ff82edf4419c4f95b4d164f69f
Hayley Davies <cd230099@falmouth.ac.uk>
2021-05-14 21:31:50 +0100

    - Created a new puzzle for the game, this is for clicking objects in a specific order as per Ryan's request. - The puzzle is ran in ClickPuzzle.cs, and each of the objects which are clickable must have the Clickable.cs script on them. - Check Click Puzzle.unity for more details on how the system works. - All scripts are commented - makes it easier to understand.

---
+++ Assets/Scripts/Click Puzzle/ClickPuzzle.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ClickPuzzle {
+    public class ClickPuzzle : MonoBehaviour {
+        // the order, and objects that will be clicked
+        public List<Clickable> clickOrder;
+
+        // the currently clicked objects
+        public List<Clickable> currentClicks;
+
+        // the message to be broadcast once the puzzle is completed
+        public string fungusCompletionMessage = "ClickPuzzleComplete";
+
+        // Start is called before the first frame update
+        void Start() {
+            // create a new list for the current clicks
+            currentClicks = new List<Clickable>(clickOrder.Count);
+
+            // add a new item for each item in the clickOrder array
+            // set the puzzle of each of the clicks to be this script
+            for (int i = 0; i < clickOrder.Count; i++) {
+                currentClicks.Add(null);
+                clickOrder[i].puzzle = this;
+            }
+        }
+
+        /// <summary>
+        /// when an object has been clicked
+        /// </summary>
+        /// <param name="clickable">the object that has been clicked</param>
+        public void ObjectClicked(Clickable clickable) {
+            // add the object to the end of the list
+            currentClicks.Add(clickable);
+            // whilst the there are more objects in the currentClicks list
+            while (currentClicks.Count > clickOrder.Count) {
+                // remove the first index
+                currentClicks.RemoveAt(0);
+            }
+
+            // check if the game is complete, if it is, run GameWin()
+            if (isComplete()) {
+                GameWin();
+            }
+        }
+
+        /// <summary>
+        /// check if the puzzle is complete
+        /// </summary>
+        /// <returns>true if the puzzle is complete</returns>
+        bool isComplete() {
+            // run through the clickOrder and currentClicks lists,
+            // if the objects aren different, return false (cuts down on O(n))
+            // if all objects are the same, return true
+            for (int i = 0; i < clickOrder.Count; i++) {
+                if (clickOrder[i] != currentClicks[i]) {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        // broadcast a fungus message/debug message
+        void GameWin() {
+            Fungus.Flowchart.BroadcastFungusMessage(fungusCompletionMessage);
+            Debug.Log(fungusCompletionMessage);
+        }
+    }
+}
---
+++ Assets/Scripts/Click Puzzle/Clickable.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ClickPuzzle {
+    public class Clickable : MonoBehaviour {
+        // the puzzle this object is part of
+        public ClickPuzzle puzzle;
+
+        /// <summary>
+        /// when the object is clicked
+        /// </summary>
+        void OnMouseDown() {
+            // run the object clicked function for the puzzle
+            puzzle.ObjectClicked(this);
+        }
+    }
+}
