d3b1b3598938bd7fd9e057e87646e839d4287c9b
Hayley Davies <cd230099@falmouth.ac.uk>
2021-05-21 01:53:17 +0100

    -MainMenu.unity scene now preloads - should make the transition to the Game scene a lot less jarring.

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -3,10 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class MainMenu : MonoBehaviour
-{
-    public void PlayGame()
-    {
+public class MainMenu : MonoBehaviour {
+
+    public PreloadSceneInUnity sceneLoad;
+    public void PlayGame() {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 }
---
+++ Assets/Scripts/PreloadSceneInUnity.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PreloadSceneInUnity : MonoBehaviour {
+    bool loaded = false;
+
+    AsyncOperation asyncLoad;
+
+    void Update() {
+        if (!loaded) {
+            // Use a coroutine to load the Scene in the background
+            StartCoroutine(LoadYourAsyncScene());
+            loaded = true;
+        }
+    }
+
+    IEnumerator LoadYourAsyncScene() {
+        yield return new WaitForSeconds(.1f);
+        asyncLoad = SceneManager.LoadSceneAsync("GAME 1");
+        asyncLoad.allowSceneActivation = false;
+
+        // yield to other processes until the scene is loaded
+        while (!asyncLoad.isDone) {
+            yield return null;
+        }
+
+        // Do something here like enabling the play button or closing the splash/loading screen
+    }
+
+    public void EnableScene() {
+        asyncLoad.allowSceneActivation = true;
+    }
+}
