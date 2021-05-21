8de796ab8840b3ec3553617c9f578f453fd679a7
Hayley Davies <cd230099@falmouth.ac.uk>
2021-05-21 06:25:23 +0100

    -Moved piece of paper into the bathroom - makes more sense and will draw the player back downstairs to complete a puzzle. -Moved pill bottle into living room

---
+++ Assets/Scripts/CreateMaterialFromInventoryItem.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using InventorySystem;
+using UnityEngine;
+
+public class CreateMaterialFromInventoryItem : MonoBehaviour {
+    private InventoryObject inventoryObject;
+    // Start is called before the first frame update
+    void Start() {
+        inventoryObject = GetComponent<InventoryObject>();
+        GetComponent<Renderer>().material.mainTexture = inventoryObject.item.Icon.texture;
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+}
